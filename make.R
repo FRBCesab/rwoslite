b588921eda7289095f7634f1ff81ea249e575fdd
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-02-01 10:55:49 +0100

    Add min make.R

---
+++ make.R
@@ -0,0 +1,29 @@
+#' R Client for WOS API
+#'
+#' @description 
+#' ...
+#' 
+#' @author Nicolas Casajus \email{nicolas.casajus@fondationbiodiversite.fr}
+#' 
+#' @date 2022/02/01
+
+
+
+## Install `remotes` package ----
+
+if (!("remotes" %in% installed.packages())) install.packages("remotes")
+
+
+## Install required packages (listed in DESCRIPTION) ----
+
+remotes::install_deps(upgrade = "never")
+
+
+## Load project dependencies ----
+
+devtools::load_all(here::here())
+
+
+## Create 'outputs' folder ----
+
+dir.create(here::here("outputs"), showWarnings = FALSE)
