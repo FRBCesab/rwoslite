89878a880d6fa4bfd6f19277167e13637f10a6a8
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-02-08 16:45:38 +0100

    Edit documentation + examples

--- R/wos-search.R
+++ R/wos-search.R
@@ -1,22 +1,65 @@
-#' Retrieve total number of publications from a request
+#' Retrieve the number of records that match a given WOS query
 #' 
-#' https://developer.clarivate.com/apis/woslite
-#'
+#' @description
+#' This function sends a query to the 
+#' [Web Of Science Lite API](https://developer.clarivate.com/apis/woslite)
+#' and returns the total number of records that match this query.
+#' 
+#' To learn how to write a WOS query, users can read the 
+#' [WOS documentation](https://images.webofknowledge.com/images/help/WOK/contents.html).
+#' A list of WOS field tags is available 
+#' [here](https://images.webofknowledge.com/images/help/WOS/hs_wos_fieldtags.html).
+#' 
+#' It's strongly recommended to use this function before [wos_get_records] to 
+#' have an idea on how many records you will download.
+#' 
 #' @param query a character of length 1. The query to send to the WOS lite API.
+#'   Visit the [WOS documentation](https://images.webofknowledge.com/images/help/WOK/contents.html) 
+#'   on how to write a query.
 #' 
-#' @param database a character of length 1. One among... See ... for further 
-#'   information.
+#' @param database a character of length 1. One among `BCI` (BIOSIS Citation 
+#'   Index), `BCI` (BIOSIS Citation Index), `BIOABS` (Biological Abstracts), 
+#'   `BIOSIS` (BIOSIS Previews), `CCC` (Current Contents Connect), `DCI` 
+#'   (Data Citation Index), `DIIDW` (Derwent Innovations Index), `MEDLINE` 
+#'   (Medline), `WOS` (Web of Science Core Collection), 
+#'   `ZOOREC` (Zoological Record), and `WOK` (all databases).
+#'   Default is `WOK` (all databases).
 #'
-#' @return The total number of publications (integer of length 1) matching the 
+#' @return The total number of records (integer of length 1) that match the 
 #'   query.
 #'   
 #' @export
 #'
 #' @examples
-#' query <- "TS=((salmo AND salar) AND conservation)"
+#' \dontrun{
+#' ## Search in TOPIC an exact expression ----
+#' query <- "TS=\"salmo salar\""
 #' wos_search(query)
+#' ## [1] 21411
+#' 
+#' ## Search in TOPIC an exact expression and another term ----
+#' query <- "TS=(\"salmo salar\" AND conservation)"
+#' wos_search(query)
+#' ## [1] 3779
+#' 
+#' ## Search for a specific year ----
+#' query <- "TS=(\"salmo salar\" AND conservation) AND PY=2021"
+#' wos_search(query)
+#' ## [1] 114
+#' 
+#' ## Search for a time span ----
+#' query <- "TS=(\"salmo salar\" AND conservation) AND PY=2010-2021"
+#' wos_search(query)
+#' ## [1] 1103
+#' 
+#' ## Search for an author ----
+#' query <- "AU=(\"Casajus N\")"
+#' wos_search(query)
+#' ## [19]
+#' }
 
 wos_search <- function(query, database = "WOK") {
+  
   
   ## URL encoding ----
   
