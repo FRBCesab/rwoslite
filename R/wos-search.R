cdc9f1b74c2bc670b372873d1677d57221f69bff
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-10-13 09:30:46 +0200

    Edit functions docs

--- R/wos-search.R
+++ R/wos-search.R
@@ -2,31 +2,28 @@
 #' 
 #' @description
 #' This function sends a query to the Web Of Science Lite API 
-#' (https://developer.clarivate.com/apis/woslite) and returns the total number 
-#' of records that match this query.
+#' (\url{https://developer.clarivate.com/apis/woslite}) and returns the total 
+#' number of records that match this query.
 #' 
-#' To learn how to write a WOS query, users can read the 
-#' WOS documentation available at:
-#' https://images.webofknowledge.com/images/help/WOK/contents.html.
-#' A list of WOS field tags is available 
-#' at https://images.webofknowledge.com/images/help/WOS/hs_wos_fieldtags.html.
+#' To learn how to write a WOS query, users can read the WOS documentation 
+#' available at:
+#' \url{https://images.webofknowledge.com/images/help/WOK/contents.html}.
+#' A list of WOS field tags is available at: 
+#' \url{https://images.webofknowledge.com/images/help/WOS/hs_wos_fieldtags.html}.
 #' 
-#' It's strongly recommended to use this function before [wos_get_records] to 
-#' have an idea on how many records you will download.
+#' @param query a `character` of length 1. The query to send to the WOS lite 
+#'   API. Visit the WOS documentation at:
+#'   \url{https://images.webofknowledge.com/images/help/WOK/contents.html} on 
+#'   how to write a WOS query.
 #' 
-#' @param query a character of length 1. The query to send to the WOS lite API.
-#'   Visit the WOS documentation at
-#'   https://images.webofknowledge.com/images/help/WOK/contents.html on how to 
-#'   write a query.
-#' 
-#' @param database a character of length 1. One among `BCI` (BIOSIS Citation 
+#' @param database a `character` of length 1. One among `BCI` (BIOSIS Citation 
 #'   Index), `BIOABS` (Biological Abstracts), `BIOSIS` (BIOSIS Previews), 
 #'   `CCC` (Current Contents Connect), `DCI` (Data Citation Index), `DIIDW` 
 #'   (Derwent Innovations Index), `MEDLINE` (Medline), `WOS` (Web of Science 
 #'   Core Collection), `ZOOREC` (Zoological Record), and `WOK` (all databases).
-#'   Default is `WOK` (all databases).
+#'   Default is `WOS` (all databases).
 #'
-#' @return The total number of records (integer of length 1) that match the 
+#' @return The total number of records (`integer` of length 1) that match the 
 #'   query.
 #'   
 #' @export
@@ -54,7 +51,7 @@
 #' wos_search(query)
 #' }
 
-wos_search <- function(query, database = "WOK") {
+wos_search <- function(query, database = "WOS") {
   
   
   ## Checks ----
